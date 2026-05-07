ed0937922c400f002c965ba9a8542f4fd4692ab4
chdtumaksym <diversionka401@gmail.com>
2026-05-07 04:52:35 +0300

    Update my_cheat.cpp

--- my_cheat.cpp
+++ my_cheat.cpp
@@ -1,40 +1,41 @@
 #include <windows.h>
-#include <fstream>
-#include <vector>
 #include <stdint.h>
 
-DWORD WINAPI LearningTrigger(LPVOID lpParam) {
+DWORD WINAPI ProTrigger(LPVOID lpParam) {
     uintptr_t client = (uintptr_t)GetModuleHandleA("client.dll");
-    uintptr_t localPlayerPawn = 0x2057720; // Твой базовый оффсет
+    uintptr_t localPlayerPawn = 0x2057720; 
     uintptr_t foundOffset = 0;
 
     while (true) {
         uintptr_t local = *(uintptr_t*)(client + localPlayerPawn);
         if (local > 0x1000000) {
             
-            // ЕСЛИ МЫ ЕЩЕ НЕ НАШЛИ ОФФСЕТ
             if (foundOffset == 0) {
-                // Если ТЫ САМ нажал ЛКМ на враге
+                // ОБУЧЕНИЕ: Нажми ЛКМ ТОЛЬКО когда прицел на враге!
                 if (GetAsyncKeyState(VK_LBUTTON) & 0x8000) {
                     for (uintptr_t i = 0x1300; i < 0x1600; i += 4) {
                         int val = *(int*)(local + i);
-                        // Если в этот момент по адресу лежит ID врага (1-64)
+                        // Ищем адрес, который равен ID врага (1-64)
                         if (val > 0 && val <= 64) {
-                            foundOffset = i; // МЫ НАШЛИ ЕГО!
-                            Beep(1000, 200); // Писк - оффсет найден!
-                            break;
+                            // Проверяем соседний байт, чтобы отсечь мусор
+                            if (*(int*)(local + i + 4) == 0) { 
+                                foundOffset = i;
+                                Beep(1000, 200); // ПИК - НАШЛИ!
+                                break;
+                            }
                         }
                     }
                 }
-            } 
-            // ЕСЛИ ОФФСЕТ УЖЕ НАЙДЕН - РАБОТАЕМ КАК ТРИГГЕР
-            else {
-                int id = *(int*)(local + foundOffset);
-                if (id > 0 && id <= 64) {
-                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-                    Sleep(10);
-                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-                    Sleep(200);
+            } else {
+                // РАБОТА: Стреляем только по окну игры
+                if (GetForegroundWindow() == FindWindowA("SDL_app", "Counter-Strike 2")) {
+                    int id = *(int*)(local + foundOffset);
+                    if (id > 0 && id <= 64) {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                        Sleep(20);
+                        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                        Sleep(200); // Пауза, чтобы не было спама
+                    }
                 }
             }
         }
@@ -44,6 +45,6 @@ DWORD WINAPI LearningTrigger(LPVOID lpParam) {
 }
 
 BOOL APIENTRY DllMain(HMODULE h, DWORD r, LPVOID p) {
-    if (r == DLL_PROCESS_ATTACH) CloseHandle(CreateThread(0, 0, LearningTrigger, 0, 0, 0));
+    if (r == DLL_PROCESS_ATTACH) CloseHandle(CreateThread(0, 0, ProTrigger, 0, 0, 0));
     return TRUE;
 }
