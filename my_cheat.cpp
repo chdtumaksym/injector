9350e7391aa2513ee7c215e49799da0f6451a3c8
chdtumaksym <diversionka401@gmail.com>
2026-05-07 12:44:38 +0300

    Update my_cheat.cpp

--- my_cheat.cpp
+++ my_cheat.cpp
@@ -1,50 +1,35 @@
 #include <windows.h>
+#include <fstream>
 #include <stdint.h>
 
-DWORD WINAPI TruthHunter(LPVOID lpParam) {
+struct Vector3 { float x, y, z; };
+
+namespace offsets {
+    // Используем твой вчерашний рабочий адрес (измени, если лог будет пустым)
+    constexpr uintptr_t dwLocalPlayerPawn = 0x2057720; 
+    constexpr uintptr_t m_vOldOrigin = 0x127C; // Оффсет координат (Source 2 стандарт)
+}
+
+DWORD WINAPI CoordThread(LPVOID lpParam) {
+    std::ofstream log("CS2_Coords_Log.txt", std::ios::app);
     uintptr_t client = (uintptr_t)GetModuleHandleA("client.dll");
-    uintptr_t localPlayerPawn = 0x2057720; 
-    uintptr_t foundOffset = 0;
+
+    log << "--- COORDINATE CHECK START ---\n";
 
     while (true) {
-        uintptr_t local = *(uintptr_t*)(client + localPlayerPawn);
-        if (local > 0x1000000) {
+        uintptr_t localPlayer = *(uintptr_t*)(client + offsets::dwLocalPlayerPawn);
+        if (localPlayer > 0x1000000) {
+            Vector3 pos = *(Vector3*)(localPlayer + offsets::m_vOldOrigin);
             
-            if (foundOffset == 0) {
-                // ШАГ 1: ОБУЧЕНИЕ (Наведи на бота и зажми ПКМ)
-                if (GetAsyncKeyState(VK_RBUTTON) & 0x8000) {
-                    for (uintptr_t i = 0x1300; i < 0x1600; i += 4) {
-                        int val = *(int*)(local + i);
-                        if (val > 0 && val <= 64) {
-                            // ШАГ 2: ПРОВЕРКА НА МУСОР
-                            // Ждем 100мс и смотрим: если ты убрал прицел, значение ДОЛЖНО стать 0
-                            Sleep(100); 
-                            if (*(int*)(local + i) == 0) { 
-                                // МЫ НАШЛИ ЕГО! Это адрес, который реагирует на наведение
-                                foundOffset = i;
-                                Beep(1200, 500); // Громкий длинный писк
-                                break;
-                            }
-                        }
-                    }
-                }
-            } else {
-                // РАБОТА: Чистый триггер
-                int id = *(int*)(local + foundOffset);
-                if (id > 0 && id <= 64) {
-                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
-                    Sleep(10);
-                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-                    Sleep(200); 
-                }
-            }
+            log << "X: " << pos.x << " Y: " << pos.y << " Z: " << pos.z << "\n";
+            log.flush();
         }
-        Sleep(1);
+        Sleep(500); // Раз в полсекунды
     }
     return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE h, DWORD r, LPVOID p) {
-    if (r == DLL_PROCESS_ATTACH) CloseHandle(CreateThread(0, 0, TruthHunter, 0, 0, 0));
+    if (r == DLL_PROCESS_ATTACH) CloseHandle(CreateThread(0, 0, CoordThread, 0, 0, 0));
     return TRUE;
 }
